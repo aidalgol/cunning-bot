6e40ed90fd2e33fa5b65a12cf3f8f446f335f75e
Ian Price <ianprice90@googlemail.com>
2013-09-03 05:04:15 +0100

    Add ddg plugin

---
+++ plugins/ddg.scm
@@ -0,0 +1,20 @@
+(define-module (cunning-bot plugins ddg)
+  #:use-module (ddg)
+  #:export ((ddg-define . define)))
+
+(define (one-word? s)
+  (not (string-index s char-whitespace?)))
+
+(define (ddg-define sender args)
+  (define args* (string-trim-both args))
+  (if (not (one-word? args*))
+      (string-append sender ": One word at a time, please.")
+      (let* ((data (zero-click (string-append "!define " args*)))
+             (definition (hash-ref data "Definition"))
+             (url-string (hash-ref data "DefinitionURL")))
+        (cond ((string-null? definition)
+               (format #f "A definition for ~a is not available" args*))
+              ((string-null? url-string) definition)
+              (else
+               (format #f "~a\nDefinition courtesy of ~a"
+                       definition url-string))))))
