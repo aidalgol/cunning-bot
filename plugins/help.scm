e6c7b7dd76250d3731d4721e90ca78433657d748
Ian Price <ianprice90@googlemail.com>
2013-09-07 18:21:18 +0100

    Mark admin commands as such in the output of help

--- plugins/help.scm
+++ plugins/help.scm
@@ -3,6 +3,8 @@
   #:use-module (cunning-bot bot)
   #:export (help))
 
+(define admin-command? (@@ (cunning-bot plugins admin) admin-command?))
+
 (define bot-command (@@ (cunning-bot bot) bot-command))
 
 (define (help bot sender args)
@@ -11,6 +13,10 @@
     (string->symbol (string-trim args)))
   (cond ((bot-command bot command-name) =>
          (lambda (var)
-           (or (object-documentation var)
+           (or (and=> (pk (object-documentation var))
+                      (lambda (doc)
+                        (if (admin-command? var)
+                            (string-append "(admin) " doc)
+                            doc)))
                (format #f "no help for command: ~a" command-name))))
         (else (format #f "no such command: ~a" command-name))))
