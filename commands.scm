02d919ac30ec0ee553670e275dc2412c2e5a60bb
Aidan Gauland <aidalgol@no8wireless.co.nz>
2014-03-26 22:24:09 +1300

    Removed ponciness

--- commands.scm
+++ commands.scm
@@ -20,7 +20,7 @@
 
 (define-public (flay bot sender args)
   (if (string-null? args)
-      "Whom do you want me to flay?"
+      "Who do you want me to flay?"
       (make-action (string-append "flays " args "."))))
 
 (define-public (say-hello bot sender args)
